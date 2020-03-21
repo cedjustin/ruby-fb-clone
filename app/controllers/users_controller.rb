716660253cddd5a211501d050f1a0293913090cf
root <root@DESKTOP-3IBCSAR.localdomain>
2020-03-21 13:07:59 +0200

    i have added create function for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
     def index
         render :new
     end
@@ -8,4 +10,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+
+        @user = User.new(user_params)
+    
+        if @user.save
+          redirect_to new_session_path
+        else
+          render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
 end
