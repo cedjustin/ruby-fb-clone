61d68e0b481e038b43dd277a3c86833f09bc1f55
root <root@DESKTOP-3IBCSAR.localdomain>
2020-03-21 12:27:38 +0200

    generated controller and model for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_100454) do
+ActiveRecord::Schema.define(version: 2020_03_21_102432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_03_21_100454) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
